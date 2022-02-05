d1cd9756d6a063c2a8cd9f9e85fc82f14fc49555
JiangLongLiu <757617012@qq.com>
2022-02-05 11:54:46 +0800

    B站带时间戳的URL到剪贴板

---
+++ bilibili/bilibili 复制带时间戳的URL.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         bilibili  获取时间戳URL，以便对照视频记录文字内容
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  先把评论加载出来，按u就能在评论区插入时间戳，并接着输入内容了。
+// @author       You
+// @match        https://www.bilibili.com/video/*
+// @icon         https://www.google.com/s2/favicons?domain=bilibili.com
+// @grant        none
+// @updateURL 
+// @license MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.addEventListener('keypress', function(e){
+        console.log(e);
+        if(e.keyCode === 116) {
+            e.preventDefault();
+            console.log(e.target.value);
+            var time = document.querySelectorAll('.bilibili-player-video-time-now')[0].innerHTML;
+            var comment = document.querySelectorAll('.ipt-txt')[0];
+           // comment.value = time + ' ';
+           // comment.focus();
+            var timeYMSArr=time.split(':');
+            var joinTimeStr='00h00m00s';
+            if(timeYMSArr.length===3){
+                 joinTimeStr=timeYMSArr[0]+'h'+timeYMSArr[1]+'m'+timeYMSArr[2]+'s';
+            }else if(timeYMSArr.length===2){
+                 joinTimeStr=timeYMSArr[0]+'m'+timeYMSArr[1]+'s';
+            }
+            var burl = window.location.href;
+            burl= burl.split('?')[0]+'?t=';
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(burl+joinTimeStr);
+            }
+        }
+    })
+    // Your code here...
+})();
